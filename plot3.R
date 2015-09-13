1641da97340fcd36b6a49212ba5edcf366e5a7fc
AlbertoTrobia <alberto.trobia@unipa.it>
2015-09-13 12:17:37 +0200

    Final submission

--- plot3.R
+++ plot3.R
@@ -1,4 +1,7 @@
-data <- read.csv("./power_consumption/household_power_consumption.txt", sep=";")
+setInternet2(use=TRUE)
+source <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(source, destfile="power_consumption.zip")
+data <- read.csv(unz("power_consumption.zip", "household_power_consumption.txt"), sep=";")
 
 dataSelection <- data[(data$Date == "1/2/2007" | data$Date == "2/2/2007"),]
 
